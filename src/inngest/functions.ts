28ee92a27caa755467c135fe79189eb81ee13583
LostoVayne <moises.barrera.dev@gmail.com>
2025-08-03 23:56:30 -0400

    feat: agregar soporte para el intérprete de código y nuevas plantillas de sandbox
    - Se añadió la dependencia @e2b/code-interpreter en package.json.
    - Se creó un script compile_page.sh para compilar la página en la plantilla de sandbox.
    - Se agregó un Dockerfile para la configuración del entorno de Next.js.
    - Se creó un archivo de configuración e2b.toml para la plantilla de sandbox.
    - Se implementaron funciones en el módulo Inngest para generar y obtener el sandbox.
    - Se añadieron nuevos archivos de logo en formato PNG y SVG.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { createAgent, grok } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSanbox } from "./utils";
+
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("generate-sandbox-id", async () => {
+      const sandox = await Sandbox.create("vibe-nextjs-test-20");
+      return sandox.sandboxId;
+    });
+
     const CodeAgent = createAgent({
       name: "code-agent",
       system:
@@ -14,6 +22,12 @@ export const helloWorld = inngest.createFunction(
 
     const { output } = await CodeAgent.run(`Escribe un snippet para: ${event.data.value}`);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSanbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
