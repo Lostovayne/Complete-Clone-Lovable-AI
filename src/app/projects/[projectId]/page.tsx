eb59a1072e9d3a23e824bb0d099458bdbf5efd97
LostoVayne <moises.barrera.dev@gmail.com>
2025-09-17 22:39:53 -0300

    feat: integrate TRPC for message prefetching based on project ID

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
 
 interface PageProps {
   params: Promise<{ projectId: string }>;
@@ -6,5 +6,8 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { projectId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({ projectId }));
+
   return <div>Project is {projectId}</div>;
 }
